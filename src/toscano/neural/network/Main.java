dd2a8c0c40599e051f9a82d250304fd40336e061
Mike Toscano <michael.andrew.toscano@gmail.com>
2017-06-21 14:08:20 -0400

    organize code into objects

--- src/toscano/neural/network/Main.java
+++ src/toscano/neural/network/Main.java
@@ -4,8 +4,33 @@ public class Main {
 	
 	public static void main(String[] args) {
 		
-		Network n = new Network(3);
-		n.train();
+		// Possible inputs
+		double[][] inputs = {
+			{0, 0},
+			{0, 1},
+			{1, 0},
+			{1, 1}
+		};
+		
+		// Expected outputs
+		double[] expected = {0, 1, 1, 0};
+		
+		// Network parameters
+		int numNeurons = 3;
+		
+		System.out.println(String.format("Creating new neural network with %d neurons expecting input of length %d", numNeurons, inputs[0].length));
+		Network n = new Network(numNeurons, inputs[0].length);
+		
+		System.out.println("Training network...");
+		n.train(100000, inputs, expected);
+		
+		System.out.println("Network fully trained!\n");
+		
+		for( int i = 0; i < inputs.length; i++ ) {
+			
+			System.out.println(String.format("%d xor %d prediction = %f. Actual value = %d", (int)inputs[i][0], (int)inputs[i][1], n.predict(inputs[i]), (int)expected[i]));
+			
+		}
 		
 	}
 
--- src/toscano/neural/network/Network.java
+++ src/toscano/neural/network/Network.java
@@ -3,37 +3,30 @@ package toscano.neural.network;
 public class Network {
 	
 	private int numNeurons;
-	private Neuron[] neurons;
 	
-	public Network(int numNeurons) {
+	// global neurons
+	private Neuron[] neurons; // hidden
+	private Neuron o; // output
+	
+	public Network(int numNeurons, int inputSize) {
 		
 		this.numNeurons = numNeurons;
+		this.initialize(inputSize);
 		
 	}
 	
-	public void train() {
-		
-		// Possible inputs
-		double[][] inputs = {
-			{0, 0},
-			{0, 1},
-			{1, 0},
-			{1, 1}
-		};
-		
-		// Expected outputs
-		double[] expected = {0, 1, 1, 0};
-		
+	private void initialize(int inputSize) {
+				
 		// Creating the neurons
 		this.neurons = new Neuron[this.numNeurons];
 		for(int i = 0; i < this.numNeurons; i++) {
 			
-			Neuron n = new Neuron(inputs.length);
+			Neuron n = new Neuron(inputSize);
 			this.neurons[i] = n;
 			
 		}
 		
-		Neuron o = new Neuron(this.numNeurons);
+		o = new Neuron(this.numNeurons);
 		
 		// Assign random weights
 		for(int i = 0; i < this.numNeurons; i++) {
@@ -44,51 +37,66 @@ public class Network {
 		
 		o.randomizeWeights();
 		
+	}
+	
+	public double predict(double[] inputs) {
+		
+		for( int n = 0; n < this.numNeurons; n++ ) {
+							
+			// each input value in this set of inputs (e.g. loop through inputs[0] to get inputs[0][0], [0][1] .... [0][p]
+			for( int p = 0; p < inputs.length; p++ ) {
+				
+				this.neurons[n].inputs[p] = inputs[p];
+				
+			}
+			
+		}
+		
+		// set the output neuron's inputs to the activated values of the hidden layer neurons
+		for(int n = 0; n < this.numNeurons; n++) {
+			
+			o.inputs[n] = this.neurons[n].output();
+			
+		}
+		
+		return o.output();
+		
+	}
+	
+	private void learn(double expected) {
+		
+		// adjust the weight of the output neuron based on its error
+		o.error = Sigmoid.derivative(o.output()) * (expected - o.output());
+		o.adjustWeights();
+		
+		// adjust the weights of the hidden neurons based on their errors
+		for(int n = 0; n < this.numNeurons; n++) {
+			
+			this.neurons[n].error = Sigmoid.derivative(this.neurons[n].output()) * o.error * o.weights[n];
+			
+		}
+		
+		for(int n = 0; n < this.numNeurons; n++) {
+			
+			this.neurons[n].adjustWeights();
+			
+		}
+		
+	}
+	
+	public void train(int numSessions, double[][] inputs, double[] expected) {
+		
 		// i training sessions
-		for( int i = 0; i < 10000; i++ ) {
+		for( int i = 0; i < numSessions; i++ ) {
 			
 			// train each possible input once per session
 			for( int j = 0; j < inputs.length; j++ ) {
 				
 				// 1) forward propagation to calculate output
-				for( int n = 0; n < this.numNeurons; n++ ) {
-									
-					// each input value in this set of inputs (e.g. loop through inputs[0] to get inputs[0][0], [0][1] .... [0][p]
-					for( int p = 0; p < inputs[j].length; p++ ) {
-						
-						this.neurons[n].inputs[p] = inputs[j][p];
-						
-					}
-					
-				}
-				
-				// set the output neuron's inputs to the activated values of the hidden layer neurons
-				for(int n = 0; n < this.numNeurons; n++) {
-					
-					o.inputs[n] = this.neurons[n].output();
-					
-				}
-				
-				System.out.println(String.format("%f xor %f = %f", inputs[j][0], inputs[j][1], o.output()));
+				this.predict(inputs[j]);
 				
 				// 2) back propatation to adjust weights
-				
-				// adjust the weight of the output neuron based on its error
-				o.error = Sigmoid.derivative(o.output()) * (expected[j] - o.output());
-				o.adjustWeights();
-				
-				// adjust the weights of the hidden neurons based on their errors
-				for(int n = 0; n < this.numNeurons; n++) {
-					
-					this.neurons[n].error = Sigmoid.derivative(this.neurons[n].output()) * o.error * o.weights[n];
-					
-				}
-				
-				for(int n = 0; n < this.numNeurons; n++) {
-					
-					this.neurons[n].adjustWeights();
-					
-				}
+				this.learn(expected[j]);
 				
 			}
 			
